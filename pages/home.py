b2b89b5e7f96960348124ba75edb4288ed2bbda1
tstifft <63744073+tstifft@users.noreply.github.com>
2025-10-29 16:13:30 -0300

    Create home page for Solidarize campaign
    Add home page for the Solidarize campaign with details on donations and collection points.

---
+++ pages/home.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+
+def render():
+    st.title("🏠 Solidarize")
+    st.write("""
+        
+    A campanha **Solidarize** ocorre anualmente e visa arrecadar alimentos e apoiar aqueles que enfrentam dificuldades em nossa comunidade.
+
+    Sua participação fará toda a diferença na vida de muitas famílias.
+
+    **Como você pode ajudar:**
+
+    📦 **Doações:** Alimentos não perecíveis.  
+    🗓️ **Período:** 10/08/2024  
+    📍 **Locais de arrecadação:** 
+        
+    - Principais Mercados da Cidade  
+    - Drive Thru
+    - **Camaquã**: Av. Jose Loureiro da Silva, 787 - Carvalho Bastos, Camaquã - RS, 96784-058
+    - **Cristal**: Rua Pedro Osório, 109 - Centro, Cristal - RS, 96195-000
+    - **Tapes**: Av. Borges de Medeiros, 156, Tapes - RS, 96760-000
+
+        
+
+    Juntos podemos :blue[mais !]
+        
+
+    """)
