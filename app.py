5882ea92d13e3a7f6977f9ae88403cc28951a0b5
tstifft <63744073+tstifft@users.noreply.github.com>
2025-11-06 10:00:41 -0300

    Refactor app.py to enhance functionality and layout

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
+import streamlit as st # type: ignore
 import pandas as pd
 import altair as alt
+import streamlit_antd_components as sac
 
-#Importando as paginas
-import pages.home as home
 
-# ==============================
-# ⚙ CONFIGURAÇÕES INICIAIS
-# ==============================
+
+
+def exibir_card_progresso(cidade,total_arrecadado,meta):
+    porcentagem = (total_arrecadado / meta) * 100
+    with st.container(border=True):
+        st.markdown(
+            f"""
+            <div style="display: flex; justify-content: space-between; align-items: center;">
+                <div>
+                    <h3 style="margin-bottom: 0px;">{cidade}</h3>
+                    <p style="font-size: 1.2em; margin-top: 0px;">{total_arrecadado:,} kg arrecadados</p>
+                </div>
+                <span style='
+                    text-align: right; 
+                    color: rgb(0, 153, 255) !important; 
+                    font-size: 2.5em; 
+                    font-weight: bold;
+                '>
+                    {porcentagem:.0f}%
+                </span>
+            </div>
+            """.replace(",", "."), 
+            unsafe_allow_html=True
+        )
+            
+        # Barra de progresso. O valor deve ser um float entre 0.0 e 1.0
+        st.progress(total_arrecadado/meta)
+
+
+
+
 st.set_page_config(
     page_title="Solidarize",
     page_icon="📱",
@@ -17,22 +43,15 @@ st.set_page_config(
 )
 
 
-# Inicializa o estado da sessão
-if 'selected_menu' not in st.session_state:
-    st.session_state.selected_menu = 'Home'
 
-
-# ==============================
-# 🎨 CSS PERSONALIZADO
-# ==============================
 
 st.markdown("""
-    <link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap' rel='stylesheet'>
-    <style>
-        * {
-	font-family: 'Inter', sans-serif;
-    }
-
+        <style>
+        .block-container {
+            padding-top: 1rem;
+            padding-bottom: 0rem;
+        }
+    
         header {visibility: hidden;}
         [data-testid="stHeader"] {display: none;}
         [data-testid="stToolbar"] {display: none;}
@@ -77,146 +96,144 @@ st.markdown("""
         section[data-testid="stVerticalBlock"] > div:has(div[data-testid="stVerticalBlock"]) {
             gap: 0;
         }
-            
-        div[data-testid="stVerticalBlock"]{
-            width: 100% !important;
-        }
-        .st-key-bottom-menu {
-            /* height: 10vh !important; */
-            position: fixed !important;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            margin: 0 !important;
-            padding: 10px 0 !important;
-            background-color: white;
-            border-top: 1px solid #e0e0e0;
-            box-shadow: 0 -2px 10px rgba(0,0,0,0.1);
-            z-index:  999;   
-            
-            height: auto !important;      
-            min-height: auto !important;  
-            padding: 0 !important;
-            margin: 0 !important;
-        }
-        .st-key-bottom-menu > div {
-          width: 100%;
+
+
+        .st-ar{
+             height: 3vh;
+            border-radius: 15px; 
         }
 
-            
-        .st-key-content-area {
-            
-        }
-            
-
-
-        /* Garantir que o option_menu não tenha padding/margens extras */
-        nav[data-testid="stHorizontalBlock"], nav.st-bt, .css-1d391kg, .css-ocqkz7 {
-            padding: 0 !important;
-            margin: 0 !important;
-        }
-
-   
-</style>
-""", unsafe_allow_html=True)
+        </style>
+        """, unsafe_allow_html=True)
 
 
 
 
-# ==============================
-# 📄 CONTEÚDO DAS PÁGINAS
-# ==============================
-with st.container(key='content-area'):    
-    if st.session_state.selected_menu == "Home":
-        home.render()
 
-    elif st.session_state.selected_menu == "Doações":
-        st.title("📦 Registro de Doações")
-        with st.form("form_doacao"):
-            nome = st.text_input("Nome do doador")
-            valor = st.number_input("Valor da doação (R$)", min_value=0.0, step=10.0)
-            enviar = st.form_submit_button("Registrar")
-            if enviar:
-                st.success(f"✅ {nome} doou R$ {valor:.2f}")
 
-    elif st.session_state.selected_menu == "Relatórios":
-        st.title("📈 Relatórios")
+st.markdown(f"<h1 style='text-align: left; color: #028BF9; font-size: 6vmax'>Solidarize 2025</h1>", unsafe_allow_html=True)
+
+
+
+
+
+btn = sac.buttons([
+    sac.ButtonsItem(label='Home'),
+    sac.ButtonsItem(label='Acompanhe'),
+    sac.ButtonsItem(label='Participe'),
+    sac.ButtonsItem(label='Prioridades'),
+    # sac.ButtonsItem(icon='apple'),
+    # sac.ButtonsItem(label='google', icon='google', color='#25C3B0'),
+    # sac.ButtonsItem(label='wechat', icon='wechat'),
+    # sac.ButtonsItem(label='disabled', disabled=True),
+    # sac.ButtonsItem(label='link', icon='share-fill', href='https://ant.design/components/button'),
+], label='', align='center', color='blue', use_container_width=True ,variant='text', size='sm') #radius='lg'
+
+# st.write(f'The selected button label is: {btn}')
+
+match btn:
+    case 'Home':
         entradas = pd.read_csv(st.secrets.gsheet.entradas).sort_values(by=['Quantidade'],ascending=False)
-        st.header("Juntos já arrecadamos  ...")
 
-        ""
+        # Resumo da campanha
+        # st.header('Saiba mais')
+        st.write("""
+        A **Solidarize** é uma campanha anual de arrecadação de alimentos, dedicada a ajudar pessoas em situação de dificuldade na comunidade.
+        """)
+
+        st.subheader("Juntos já arrecadamos  ...")
+
         col1,col2 = st.columns(2)
         
-        total_kgs = '{0:,}'.format(entradas["Quantidade"].sum()).replace(',','.')
-        
-        
+        total_kgs = '{0:,}'.format(entradas["Quantidade"].sum()).replace(',','.')   
+
         tile1 = col1.container(height=200)
-        tile1.markdown(f"<h1 style='text-align: center; color: #028BF9; font-size:70px'>{total_kgs}</h1>", unsafe_allow_html=True)
+        tile1.markdown(f"<h1 style='text-align: center; color: #028BF9; font-size:65px'>{total_kgs}</h1>", unsafe_allow_html=True)
         ""
-        tile1.markdown("<h3 style='text-align: right; color: #028BF9;'>Quantidade (Kgs)</h3>", unsafe_allow_html=True)
+        tile1.markdown("<h4 style='text-align: right; color: #028BF9;'>Quantidade (Kgs)</h4>", unsafe_allow_html=True)
     
 
         total_cestas = '{0:,}'.format(entradas["Cestas"].sum()).replace(',','.')
 
 
         tile2 = col2.container(height=200)
-        tile2.markdown(f"<h1 style='text-align: center; color: #028BF9; font-size:70px'>{total_cestas}</h1>", unsafe_allow_html=True)
-        tile2.markdown("<h3 style='text-align: right; color: #028BF9;'>Cestas Básicas Montadas</h3>", unsafe_allow_html=True)
-
-        "veja por localidade aqui 	:point_down:"
-        ""
-        ""
-        ""
-    
-        # Gráfico de barras com Altair
-        # color='blues'
-        chart = alt.Chart(entradas).mark_bar().encode(
-            x=alt.X('Localidade:N',axis=alt.Axis(title='') ),
-            y=alt.Y('Quantidade:Q',axis=alt.Axis(labels=False, title='Qtd em KGs')),
-            tooltip=['Localidade', 'Quantidade']
-        ).properties(
-            title='Quantidade (kgs) por Localidade'
-        )
-        st.altair_chart(chart, use_container_width=True)
-
-        chart2 = alt.Chart(entradas).mark_bar().encode(
-            x=alt.X('Localidade:N',axis=alt.Axis(title='') ),
-            y=alt.Y('Cestas:Q',axis=alt.Axis(labels=False, title='Qtd Cestas')),
-            tooltip=['Localidade', 'Cestas']
-        ).properties(
-            title='Quantidade de Cestas por Localidade'
-        )
-        st.altair_chart(chart2, use_container_width=True)
-
-    elif st.session_state.selected_menu == "Config":
-        st.title("⚙ Configurações")
-        st.radio("Tema:", ["Claro", "Escuro"])
+        tile2.markdown(f"<h1 style='text-align: center; color: #028BF9; font-size:65px'>{total_cestas}</h1>", unsafe_allow_html=True)
+        tile2.markdown("<h4 style='text-align: right; color: #028BF9;'>Cestas Básicas Montadas</h4>", unsafe_allow_html=True)
 
 
 
-# ==============================
-# 🧭 MENU INFERIOR (FIXO)
-# ==============================
-with st.container(key='bottom-menu'):
-  selected = option_menu(
-      menu_title=None,
-      options=["Home", "Doações", "Relatórios", "Config"],
-      icons=["house", "box", "bar-chart", "gear"],
-      orientation="horizontal",
-      default_index=0,
-      key="bottom_menu",
-      styles={ "container": { "width": "100% !important" } 
-          # "container": { "padding": "1 !important", "bottom": "0 !important", "margin": "0 !important", "left":"0 !important"}
-      }
-      #   # "icon": {"color": "orange", "font-size": "25px"}, 
-      #   # "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-      #   # "nav-link-selected": {"background-color": "green"},
-      # }
-  )
-  
+    case 'Acompanhe':
+        """
+        ### Nossa meta é ultrapassar os valores arrecadados em 2024
+        """
+        entradas = pd.read_csv(st.secrets.gsheet.entradas).sort_values(by=['Quantidade'],ascending=False)
+        print(entradas)
+
+        for index, row in entradas.iterrows():
+            # Você acessa os dados usando o nome da coluna na 'row'
+            localidade = row['Localidade']
+            quantidade = row['Quantidade']
+            cestas = row['Cestas']
+            meta = row['Meta']
+            exibir_card_progresso(localidade,quantidade,meta)
+
+    case 'Participe':
+        """        
+        
+        Sua participação fará toda a diferença na vida de muitas famílias.
+
+        **Como você pode ajudar:**
+
+        📦 **Doações:** Alimentos não perecíveis.  
+        🗓️ **Período:** 08/11/2025  
+        📍 **Locais de arrecadação:** 
+                
+        - Principais Mercados da Cidade  
+        - Drive Thru
+            - **Camaquã**: Av. Jose Loureiro da Silva, 787 - Carvalho Bastos, Camaquã - RS, 96784-058
+            - **Cristal**: Rua Pedro Osório, 109 - Centro, Cristal - RS, 96195-000
+            - **Tapes**: Av. Borges de Medeiros, 156, Tapes - RS, 96760-000
+            
+                
+
+        Juntos podemos :blue[mais !] 
+        
+        
+        """
+    case 'Prioridades':
+        ############## ITENS PRIORITARIOS
+        st.header("Itens Prioritários")
+        necessidades = pd.read_csv(st.secrets.gsheet.necessidades, index_col='Localidade')
+
+        # "Esses são os itens prioritários em cada localidade"
+        ":red[*Isso nos ajuda a termos uma distribuição adqueada para montagem das cestas básicas*]"
+        
+        locais = necessidades.index.unique()
+        
+        for localidade in locais:
+            # Imprime o nome da localidade (o cabeçalho)
+        
+            st.subheader(f"\n{localidade}")
+
+            # Filtra o DataFrame original para obter apenas as linhas dessa localidade
+            # .loc[] é usado para acessar por índice
+            produtos_da_localidade = necessidades.loc[localidade, 'Produto']
+
+            # Se houver apenas 1 produto, ele não vem como uma Series.
+            # Transformamos em lista para garantir que sempre podemos iterar.
+            if isinstance(produtos_da_localidade, pd.Series):
+                lista_produtos = produtos_da_localidade.tolist()
+            else:
+                # Caso seja um único valor (string)
+                lista_produtos = [produtos_da_localidade]
+
+            # st.write(lista_produtos)
+            for prod in lista_produtos:
+                st.markdown(f"""\t- {prod}""")
+            
 
 
-# Atualiza o estado quando o menu muda
-if selected != st.session_state.selected_menu:
-  st.session_state.selected_menu = selected
-  st.rerun()
+
+
+
+
